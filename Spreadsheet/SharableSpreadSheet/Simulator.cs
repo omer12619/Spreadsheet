8ee498ebc10270228448b3b6267bf0c700cf8c31
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-05-28 17:52:25 +0300

    change

--- Spreadsheet/SharableSpreadSheet/Simulator.cs
+++ Spreadsheet/SharableSpreadSheet/Simulator.cs
@@ -8,7 +8,22 @@ namespace SharableSpreadSheet
 {
     internal class Simulator
     {
-        SharableSpreadSheet SharableSpreadSheet = new SharableSpreadSheet();
+        private int rows;
+        private int columns;
+        private SharableSpreadSheet sharableSpreadSheet;
+        private int usres;
+        private int nOperations;
+        private int mssleep;
 
+        public Simulator(int rows, int columns, int usres, int nOperations, int mssleep)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            sharableSpreadSheet = new SharableSpreadSheet(rows,columns,usres);
+            sharableSpreadSheet.load("TextFile1.txt");
+            this.usres = usres;
+            this.nOperations = nOperations;
+            this.mssleep = mssleep;
+        }
     }
 }
